356f073ef38c6ee713c3012803814fde3ea5bc79
Naahee Mohamed <naahee.mohamedd@gmail.com>
2026-06-14 07:36:01 +0000

    https://imagescdn.designweek.co.uk/uploads/2023/05/22133229/FWC26_Offici

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imagescdn.designweek.co.uk',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
